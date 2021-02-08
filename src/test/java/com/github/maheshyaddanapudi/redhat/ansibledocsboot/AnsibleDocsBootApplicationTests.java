f5601983b44a8f4732e2c26ce9789699cc286093
Mahesh Yaddanapudi <zzzmahesh@gmail.com>
2021-02-08 19:52:17 +0530

    Init

--- src/test/java/com/github/maheshyaddanapudi/redhat/ansibledocsboot/AnsibleDocsBootApplicationTests.java
+++ src/test/java/com/github/maheshyaddanapudi/redhat/ansibledocsboot/AnsibleDocsBootApplicationTests.java
@@ -3,20 +3,21 @@ package com.github.maheshyaddanapudi.redhat.ansibledocsboot;
 import com.github.maheshyaddanapudi.redhat.ansibledocsboot.constants.Constants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {FlywayAutoConfiguration.class})
 @ActiveProfiles(profiles = {Constants.TEST, Constants.MARIADB4J})
 public class AnsibleDocsBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String args[] = {"--spring.output.ansi.enabled=always"};
-		SpringApplication.run(AnsibleDocsBootApplication.class, args);
+
 	}
 
 }
