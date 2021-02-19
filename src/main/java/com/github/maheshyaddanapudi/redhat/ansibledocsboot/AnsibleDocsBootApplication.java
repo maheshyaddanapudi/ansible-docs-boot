3d12584112878b46ee46379fa543911bb824f185
Mahesh Yaddanapudi <zzzmahesh@gmail.com>
2021-02-19 14:14:21 +0530

    Update AnsibleDocsBootApplication.java

--- src/main/java/com/github/maheshyaddanapudi/redhat/ansibledocsboot/AnsibleDocsBootApplication.java
+++ src/main/java/com/github/maheshyaddanapudi/redhat/ansibledocsboot/AnsibleDocsBootApplication.java
@@ -1,14 +1,16 @@
 package com.github.maheshyaddanapudi.redhat.ansibledocsboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceHealthContributorAutoConfiguration.class, DataSourceAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
 @EnableCaching
 @EnableTransactionManagement
 public class AnsibleDocsBootApplication {
